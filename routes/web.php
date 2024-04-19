d9caa04c5f2f2da38736ed825b2af508039ae3e2
minami <sec0415.minami@gmail.com>
2024-04-19 02:36:49 +0000

    11　課題完了

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,7 @@ Route::controller(ProfileController::class)->prefix('admin')->group(function() {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
